1efa74bb0a170548dab6e54cb8cadcceb3395f0d
biniam1990 <biniamw1990@gmail.com>
2017-04-17 15:22:55 -0500

    excercises

---
+++ ex1.ts
@@ -0,0 +1,21 @@
+type BankAccount = {money:number,deposit:(value:number)=>void}
+type person = {name:string,account:BankAccount,hobbies:string[]};
+
+let bankAccount = {
+    money:2000,
+    deposit:function(value){
+        this.money +=value;
+    }
+}
+
+let asaad:person = {
+    name:"Asaad",
+    account:bankAccount,
+    hobbies:["Violin","Cooking"]
+}
+
+let berhanu:person = {
+    name:"Biniam",
+    account:bankAccount,
+    hobbies:["volley ball","reading"]
+}
